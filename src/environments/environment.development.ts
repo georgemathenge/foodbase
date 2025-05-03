344fd09b9facff3dd1104401e291faf4d0a4be0b
George <georgemathenge957@gmail.com>
2025-05-03 22:55:10 +0300

    fixed slight error with WEB_URL  resolve

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
       // BASE_URL : window["env"]["api"] ,
       BASE_URL : 'http://13.50.101.10:4007',
-      WEB_URL : window["env"]["url"],
+      // WEB_URL : window["env"]["url"],
 };
