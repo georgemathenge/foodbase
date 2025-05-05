6a62740e7ea24ae918f3800c0e141c0e724d1d93
George Mathenge <47426850+georgemathenge@users.noreply.github.com>
2025-05-05 19:19:21 +0300

    Update config.js added https to request url

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,5 +1,5 @@
 (function (window) {
     window["env"] = window["env"] || {};
-    window["env"]["api"] = "http://foodbase.duckdns.org:4007/";
+    window["env"]["api"] = "https://foodbase.duckdns.org/";
     window["env"]["url"] = "http://localhost:4200/#/"
 })(this);
