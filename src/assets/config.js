f81cac0169d14d1f09c6a59a76bef5fe0f31e1c7
George Mathenge <47426850+georgemathenge@users.noreply.github.com>
2025-05-05 18:55:42 +0300

    Update for api base url

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,5 +1,5 @@
 (function (window) {
     window["env"] = window["env"] || {};
-    window["env"]["api"] = "http://13.50.101.10:4007/";
+    window["env"]["api"] = "http://foodbase.duckdns.org:4007/";
     window["env"]["url"] = "http://localhost:4200/#/"
 })(this);
